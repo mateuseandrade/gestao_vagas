24f400114f497ce1cfcd3a02f9e6e2f0bc2d260b
Mateus <mateuseandrade@gmail.com>
2024-06-04 21:21:28 -0300

    Criando repositorio

---
+++ src/main/java/br/com/mateusandrade/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -0,0 +1,7 @@
+package br.com.mateusandrade.gestao_vagas.modules.candidate;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID>{
+  
+}
